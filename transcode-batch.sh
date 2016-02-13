84ff5863e82fc6a67ad2fe9bbea89ebc571a2da7
Alex Du Bois <alex.dubois@mac.com>
2016-02-13 10:24:51 -0600

    Refactored into functions

--- transcode-batch.sh
+++ transcode-batch.sh
@@ -58,17 +58,6 @@ die() {
 	exit ${2:-1}
 }
 
-# Set global variables
-#
-readonly input="$1"
-readonly title_name="$(basename "$1" | sed 's/\.[^.]*$//')"
-readonly base_name=`echo $title_name | sed 's/_[^_]*$//'`
-readonly crop_dir="_crops/$base_name"
-readonly crop_file="$crop_dir/${title_name}.txt"
-readonly originals_dir="_originals/$base_name"
-readonly finals_dir="_finals/$base_name"
-readonly logs_dir="_logs/$base_name"
-
 if [ ! "$input" ]; then
 	syntax_error 'too few arguments'
 fi
@@ -77,61 +66,79 @@ if [ ! -e "$input" ]; then
 	die "input not found: $input"
 fi
 
-## Run crop detection and save output to file
+# Set global variables
 #
-if [ ! -d $crop_dir ]; then
-	mkdir $crop_dir
-fi
+PATH="/usr/local/bin:$PATH"
+readonly input="$1"
+readonly work_dir=`dirname "$input"`
+readonly title_name="$(basename "$input" | sed 's/\.[^.]*$//')"
+readonly base_name=`echo $title_name | sed 's/_[^_]*$//'`
+readonly crop_dir="_crops/$base_name"
+readonly crop_file="$crop_dir/${title_name}.txt"
+readonly originals_dir="_originals/$base_name"
+readonly finals_dir="_finals/$base_name"
+readonly logs_dir="_logs/$base_name"
+readonly media_info=`transcode-video --scan $input`
 
-detect-crop.sh $1 &> $crop_file
+crop_options=''
+video_options=''
+audio_options=''
+subtitle_options=''
+logging_options='--quiet'
 
-if [[ -f "$crop_file" ]] && [[ `grep identical $crop_file` ]]; then
-	crop_option="--crop `grep transcode-video $crop_file | egrep -o [0-9]+:[0-9]+:[0-9]+:[0-9]+`"
+function setCroppingOptions() {
+  if [ ! -d "_crops" ]; then
+    mkdir "_crops"
+  fi
+
+  if [ ! -d "$crop_dir" ]; then
+  	mkdir "$crop_dir"
+  fi
+
+  detect-crop "$input" &> "$crop_file"
+
+  if [[ -f "$crop_file" ]] && [[ ! `grep differ $crop_file` ]]; then
+  	crop_options=`grep transcode-video $crop_file | egrep -o -e "--crop "[0-9]+:[0-9]+:[0-9]+:[0-9]+`
+  else
+  	crop_options=''
+  fi
+}
+
+function setVideoOptions() {
+  video_options="--max-width 1920 --max-height 1080"
+}
+
+function setAudioOptions() {
+  if [ `echo "$media_info" | egrep "(\d\.\d ch)" | wc -l` -gt 10 ]; then
+    audio_options="--add-audio language=eng"
+  else
+    audio_options="--add-audio all"
+  fi
+
+  audio_options="$audio_options --audio-width all=double"
+}
+
+function setSubtitleOptions() {
+  srt_file=`ls "$work_dir" | egrep '\.(srt)$'`
+
+  if [ -n "$srt_file" ]; then
+    subtitle_options="--add-srt $work_dir/$srt_file"
+  elif [ `echo "$media_info" | egrep "English \(iso639-2: eng\)" | wc -l` -gt 0 ]; then
+    subtitle_options='--add-subtitle language=eng'
+  else
+    subtitle_options=''
+  fi
+}
+
+if [ -f "$input" ]; then
+  setCroppingOptions
+  setVideoOptions
+  setAudioOptions
+  setSubtitleOptions
+
+  transcode-video --mp4 $crop_options $video_options $audio_options $subtitle_options $logging_options "$input"
+
+  cleanup
 else
-	crop_option=''
+  echo "There was a problem with the file you selected."
 fi
-
-# Detect audio streams
-#
-audio_streams=`ffmpeg -i $input 2>&1 | grep -c Audio:`
-
-if [ "$audio_streams" -gt 1 ]; then
-  for i in `seq 1 $audio_streams`; do
-	audio_options="$audio_options --add-audio $i"
-  done
-else
-  audio_options=''
-fi
-
-# Detect subtitles
-#
-subtitle_streams=`ffmpeg -i $input 2>&1 | grep -v pgs | grep -c Subtitle:`
-
-if [ "$subtitle_streams" -gt 0 ]; then
-  for i in `seq 1 $subtitle_streams`; do
-	subtitle_options="$subtitle_options --add-subtitle $i"
-  done
-else
-  subtitle_options=''
-fi
-
-# Begin transcode operation
-#
-transcode-video.sh --allow-ac3 $audio_options $crop_option $input
-
-# Clean up source and generated files
-#
-if [ ! -d "$originals_dir" ]; then
-  mkdir "$originals_dir"
-fi
-mv $input "$originals_dir"
-
-if [ ! -d "$logs_dir" ]; then
-  mkdir "$logs_dir"
-fi
-mv "$title_name.mp4.log" "$logs_dir"
-
-if [ ! -d "$finals_dir" ]; then
-  mkdir "$finals_dir"
-fi
-mv "$title_name.mp4" "$finals_dir"
